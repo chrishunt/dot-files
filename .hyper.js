e9883874577bcd6ed737edf1cfa103922442eabc
Chris Hunt <huntca@gmail.com>
2019-04-10 00:00:00 +0000

    Update hyper.js for Windows

--- .hyper.js
+++ .hyper.js
@@ -103,7 +103,7 @@ module.exports = {
     //
     // PowerShell on Windows
     // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
-    shell: '',
+    shell: 'C:\\Windows\\System32\\bash.exe',
 
     // for setting shell arguments (i.e. for using interactive shellArgs: `['-i']`)
     // by default `['--login']` will be used
